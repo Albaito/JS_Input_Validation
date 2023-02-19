d9d389dcf637dd972a0823527225e166da71cea9
Albaito <121689899+Albaito@users.noreply.github.com>
2023-02-19 14:23:36 -0600

    Create Input_Validation.js

---
+++ Input_Validation.js
@@ -0,0 +1,31 @@
+
+ function isPresent(value, name){
+    if (value.length < 1 || value == ''){
+        return 'Input missing from ' + name + ' please ensure a value is entered'
+    }
+    return ''
+ }
+
+function isInteger(value, name) {
+    if (Number.isInteger(value)){
+        return ''
+    }
+    return name + ' is a non-whole number, please enter only whole numbers for ' + name
+}
+
+ function isNumeric(value, name){
+    if (isNaN(value)){
+        return name + ' is non-numeric, please enter a number for ' + name
+    }
+    return ''
+ }
+
+ function isWithinRange(min, max, value, name){
+    if (value < min ){
+        return name + ' is smaller than minimum value: ' + min
+    }
+    else if (value > max){
+        return name + ' is larger than maximum value: ' + max
+    }
+    return ''
+ }
